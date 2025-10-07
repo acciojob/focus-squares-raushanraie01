a650d541c7a524c37af109749d2b1d439571aee1
Raushan Kumar <113075873+raushanraie01@users.noreply.github.com>
2025-10-07 09:47:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+      let boxes = document.querySelectorAll(".square");
+      boxes.forEach((box) => {
+        box.addEventListener("mouseover", function handleSquare() {
+          let sq1 = document.getElementById("square1");
+          let sq2 = document.getElementById("square2");
+          let sq3 = document.getElementById("square3");
+          for (let box of boxes) {
+            if (box.id != this.id) {
+              box.style.backgroundColor = "#6a5acd"; /*slateblue*/
+            }
+          }
+        });
+        box.addEventListener("mouseout", function handleSquare() {
+            boxes.forEach(box=>{
+          box.style.backgroundColor = "#E6E6FA";
+
+            })
+        });
+      });
